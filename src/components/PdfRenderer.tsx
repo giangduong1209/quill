99b5151df180ca89091408e9ae84179fe99f12d1
chainman2209 <chainsawman2209@gmail.com>
2024-01-03 00:06:31 +0700

    feat: config rendering pdf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Document as DocumentPDF, Page } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
-const PdfRenderer = () => {
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5"></div>
+        <div className="flex items-center gap-1.5">top bar</div>
+        <div className="flex-1 w-full max-h-screen">
+          <div>
+            <DocumentPDF className="max-h-full">
+              <Page pageNumber={1}></Page>
+            </DocumentPDF>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/UploadFileBtn.tsx
+++ src/components/UploadFileBtn.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 import DropZone from "react-dropzone";
-import { Cloud, File } from "lucide-react";
+import { Cloud, File, Loader2 } from "lucide-react";
 import { Progress } from "./ui/progress";
 import { useUploadThing } from "@/lib/uploadthing";
 import { useToast } from "./ui/use-toast";
@@ -109,9 +109,18 @@ const UploadDropzone = () => {
               {isLoading ? (
                 <div className="w-full mt-4 max-w-xs mx-auto">
                   <Progress
+                    indicatorColor={
+                      uploadProgress === 100 ? "bg-green-500" : ""
+                    }
                     value={uploadProgress}
                     className="h-1 w-full bg-zinc-200"
                   />
+                  {uploadProgress === 100 ? (
+                    <div className="flex gap-1 items-center justify-center text-zinc-700 text-sm text-center pt-2">
+                      <Loader2 className="h-3 w-3 animate-spin" />
+                      Redirecting...
+                    </div>
+                  ) : null}
                 </div>
               ) : null}
 
